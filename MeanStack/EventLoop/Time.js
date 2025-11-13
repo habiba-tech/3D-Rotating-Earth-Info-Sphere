87fc53dc3bfc4ac421ad7ff038d6be33d4545d5a
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2025-11-13 10:11:47 +0530

    Create Time.js

---
+++ MeanStack/EventLoop/Time.js
@@ -0,0 +1,11 @@
+//AIM : To write a node.js program to demonstrated the event loop mechanism
+
+// Using setTimeout to log after 2 seconds
+console.log("A");
+setTimeout(() => {
+
+    console.log("Habiba (after 2 seconds)");
+}, 2000);   
+
+console.log("B");
+
